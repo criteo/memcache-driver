8589ff68dcc0561477c9c600d17fd94c2543ef0f
Sebastien FOUTRIER <s.foutrier@criteo.com>
2013-04-17 17:44:32 +0200

    Merged SocketBase and SynchronousSocket

--- Criteo.Memcache/Transport/MemcacheSocket.cs
+++ Criteo.Memcache/Transport/MemcacheSocket.cs
@@ -15,20 +15,253 @@ using Criteo.Memcache.Exceptions;
 
 namespace Criteo.Memcache.Transport
 {
-    internal class MemcacheSocketSynchronous : MemcacheSocketBase, IMemcacheTransport
+    internal class MemcacheSocket : IMemcacheTransport
     {
-        private CancellationTokenSource _token;
-        private ConcurrentQueue<IMemcacheRequest> _pending = new ConcurrentQueue<IMemcacheRequest>();
-        private volatile Action<MemcacheSocketSynchronous> _setupAction;
-        private bool _threadedReceived = false;
-
-        public MemcacheSocketSynchronous(EndPoint endpoint, IMemcacheAuthenticator authenticator, IMemcacheNode node, int queueTimeout, int pendingLimit, Action<MemcacheSocketSynchronous> setupAction, bool threaded)
-            : base(endpoint, authenticator, queueTimeout, pendingLimit, null, node)
+        #region Events
+        protected Action<Exception> _transportError;
+        public event Action<Exception> TransportError
         {
+            add
+            {
+                _transportError += value;
+            }
+            remove
+            {
+                _transportError -= value;
+            }
+        }
+
+        protected Action<MemcacheResponseHeader, IMemcacheRequest> _memcacheError;
+        public event Action<MemcacheResponseHeader, IMemcacheRequest> MemcacheError
+        {
+            add
+            {
+                _memcacheError += value;
+            }
+            remove
+            {
+                _memcacheError -= value;
+            }
+        }
+
+        protected Action<MemcacheResponseHeader, IMemcacheRequest> _memcacheResponse;
+        public event Action<MemcacheResponseHeader, IMemcacheRequest> MemcacheResponse
+        {
+            add
+            {
+                _memcacheResponse += value;
+            }
+            remove
+            {
+                _memcacheResponse -= value;
+            }
+        }
+        #endregion Event
+
+        private readonly bool _threadedReceived;
+        private readonly int _queueTimeout;
+        // TODO : add me in the Conf
+        private readonly int _requestLimit = 1000;
+        private readonly int _windowSize = 2 << 15;
+        // END TODO
+        private readonly EndPoint _endPoint;
+        private readonly IMemcacheAuthenticator _authenticator;
+        private readonly Action<MemcacheSocket> _setupAction;
+        private readonly Timer _reconnectTimer;
+
+        private bool _disposed = false;
+        private bool _initialized = false;
+        private BlockingCollection<IMemcacheRequest> _pendingRequests;
+        private Socket _socket;
+        private CancellationTokenSource _token;
+        private Timer _disposeAttemptTimer;
+
+        /// <summary>
+        /// Ctor, intialize things ...
+        /// </summary>
+        /// <param name="endpoint" />
+        /// <param name="authenticator">Object that ables to Sasl authenticate the socket</param>
+        /// <param name="queueTimeout" />
+        /// <param name="pendingLimit" />
+        /// <param name="setupAction">Delegate to call when the transport is alive</param>
+        /// <param name="threaded">If true use a thread to synchronously receive on the socket else use the asynchronous API</param>
+        public MemcacheSocket(EndPoint endpoint, IMemcacheAuthenticator authenticator, int queueTimeout, int pendingLimit, Action<MemcacheSocket> setupAction, bool threaded)
+        {
+            _endPoint = endpoint;
+            _authenticator = authenticator;
+            _queueTimeout = queueTimeout;
             _setupAction = setupAction;
             _threadedReceived = threaded;
+            _reconnectTimer = new Timer(TryReconnect);
 
             Reset(null);
+        }
+
+        /// <summary>
+        /// Synchronously sends a request
+        /// </summary>
+        /// <param name="request" />
+        public bool TrySend(IMemcacheRequest request)
+        {
+            if (request == null)
+                return false;
+
+            if (!_initialized && !Initialize())
+                return false;
+
+            return SendRequest(request);
+        }
+
+        /// <summary>
+        /// Program a call to the setup action when the transport will be up again
+        /// </summary>
+        public void PlanSetup()
+        {
+            if (_initialized && _setupAction != null)
+                // the transport is already up => execute now
+                _setupAction(this);
+        }
+
+        /// <summary>
+        /// Dispose the socket
+        /// 5 clean tries with 1 second interval, after it dispose it in a more dirty way
+        /// </summary>
+        public virtual void Dispose()
+        {
+            // block the start of any resets, then shut down
+            lock (this)
+            {
+                _disposed = true;
+                int attempt = 0;
+
+
+                _disposeAttemptTimer = new Timer(socket =>
+                {
+                    try
+                    {
+                        lock (this)
+                        {
+                            if (_pendingRequests.Count == 0 || ++attempt > 5)
+                            {
+                                try
+                                {
+                                    ShutDown();
+                                }
+                                finally
+                                {
+                                    // fail of pending requests
+                                    if (_pendingRequests != null)
+                                    {
+                                        DisposePending(_pendingRequests);
+                                        _pendingRequests = null;
+                                    }
+                                    _disposeAttemptTimer.Dispose();
+                                    _disposeAttemptTimer = null;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception e2)
+                    {
+                        if (_transportError != null)
+                            _transportError(e2);
+                    }
+                }, null, 1000, 1000);
+
+            }
+        }
+
+        private void CreateSocket()
+        {
+            var socket = new Socket(_endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(_endPoint);
+
+            // Set me in conf
+            socket.ReceiveBufferSize = _windowSize;
+            socket.SendBufferSize = _windowSize;
+
+            if (_socket != null)
+                _socket.Dispose();
+            _socket = socket;
+        }
+
+        private void Reset(Socket socketFailing)
+        {
+            BlockingCollection<IMemcacheRequest> oldPending = null;
+
+            lock (this)
+            {
+                // the transport has been disposed or the socket has already changed
+                if (_disposed ||
+                    (_socket != null && !object.ReferenceEquals(socketFailing, _socket)))
+                    return;
+
+                // somthing goes wrong, stop to send
+                ShutDown();
+                _initialized = false;
+
+                // keep the pending request somewhere
+                var newPending = _requestLimit > 0 ?
+                    new BlockingCollection<IMemcacheRequest>(new ConcurrentQueue<IMemcacheRequest>(), _requestLimit) :
+                    new BlockingCollection<IMemcacheRequest>(new ConcurrentQueue<IMemcacheRequest>());
+                oldPending = Interlocked.Exchange(ref _pendingRequests, newPending);
+            }
+
+            if (oldPending != null)
+                DisposePending(oldPending);
+        }
+
+        private void DisposePending(BlockingCollection<IMemcacheRequest> pending)
+        {
+            IMemcacheRequest request;
+            while (pending.TryTake(out request))
+                request.Fail();
+            pending.Dispose();
+        }
+
+        private bool Initialize()
+        {
+            try
+            {
+                lock (this)
+                {
+                    if (!_initialized && !_disposed)
+                    {
+                        CreateSocket();
+                        Start();
+
+                        _initialized = true;
+                    }
+                }
+            }
+            catch (Exception e2)
+            {
+                if (_transportError != null)
+                    _transportError(e2);
+
+                _reconnectTimer.Change(1000, Timeout.Infinite);
+            }
+
+            return _initialized;
+        }
+
+        private IMemcacheRequest UnstackToMatch(MemcacheResponseHeader header)
+        {
+            IMemcacheRequest result = null;
+
+            if (header.Opcode.IsQuiet())
+            {
+                throw new MemcacheException("No way we can match a quiet request !");
+            }
+            else
+            {
+                if (!_pendingRequests.TryTake(out result))
+                    throw new MemcacheException("Received a response when no request is pending");
+                if (result.RequestId != header.Opaque)
+                    throw new MemcacheException("Received a response that doesn't match with the sent request queue");
+            }
+
+            return result;
         }
 
         #region Threaded Reads
@@ -42,7 +275,7 @@ namespace Criteo.Memcache.Transport
                 var token = (CancellationToken)t;
                 while (!token.IsCancellationRequested)
                 {
-                    var socket = Socket;
+                    var socket = _socket;
                     try
                     {
                         int received = 0;
@@ -89,7 +322,7 @@ namespace Criteo.Memcache.Transport
                     }
                     catch (Exception e)
                     {
-                        if (!token.IsCancellationRequested && !Disposed)
+                        if (!token.IsCancellationRequested && !_disposed)
                         {
                             if (_transportError != null)
                                 _transportError(e);
@@ -115,7 +348,7 @@ namespace Criteo.Memcache.Transport
 
         private void ReadResponse()
         {
-            var socket = Socket;
+            var socket = _socket;
             try
             {
                 _receiveArgs.SetBuffer(0, MemcacheResponseHeader.SIZE);
@@ -124,7 +357,7 @@ namespace Criteo.Memcache.Transport
             }
             catch (Exception e)
             {
-                if (!Disposed)
+                if (!_disposed)
                 {
                     if (_transportError != null)
                         _transportError(e);
@@ -196,7 +429,7 @@ namespace Criteo.Memcache.Transport
             }
             catch (Exception e)
             {
-                if (!Disposed)
+                if (!_disposed)
                 {
                     if (_transportError != null)
                         _transportError(e);
@@ -206,7 +439,7 @@ namespace Criteo.Memcache.Transport
         }
         #endregion Async Reads
 
-        protected override void Start()
+        private void Start()
         {
             _token = new CancellationTokenSource();
 
@@ -221,7 +454,7 @@ namespace Criteo.Memcache.Transport
             Authenticate();
         }
 
-        protected override void ShutDown()
+        private void ShutDown()
         {
             try
             {
@@ -233,7 +466,7 @@ namespace Criteo.Memcache.Transport
                 _transportError(e);
             }
 
-            var socket = Socket;
+            var socket = _socket;
             if (socket != null)
                 socket.Dispose();
 
@@ -246,17 +479,19 @@ namespace Criteo.Memcache.Transport
             bool authDone = false;
             IMemcacheRequest request = null;
             Status authStatus = Status.NoError;
-            if (AuthenticationToken != null)
+
+            if (_authenticator != null)
             {
-                while (!authDone)
+                var authenticationToken = _authenticator.CreateToken();
+                while (authenticationToken != null && !authDone)
                 {
-                    authStatus = AuthenticationToken.StepAuthenticate(out request);
+                    authStatus = authenticationToken.StepAuthenticate(out request);
 
                     switch (authStatus)
                     {
                         // auth OK, clear the token
                         case Status.NoError:
-                            AuthenticationToken = null;
+                            authenticationToken = null;
                             authDone = true;
                             break;
                         case Status.StepRequired:
@@ -264,14 +499,14 @@ namespace Criteo.Memcache.Transport
                             {
                                 if (_transportError != null)
                                     _transportError(new AuthenticationException("Unable to authenticate : step required but no request from token"));
-                                Reset(Socket);
+                                Reset(_socket);
                                 return false;
                             }
                             return SendRequest(request);
                         default:
                             if (_transportError != null)
                                 _transportError(new AuthenticationException("Unable to authenticate : status " + authStatus.ToString()));
-                            Reset(Socket);
+                            Reset(_socket);
                             return false;
                     }
                 }
@@ -280,59 +515,32 @@ namespace Criteo.Memcache.Transport
             return true;
         }
 
-        /// <summary>
-        /// Synchronously sends the request
-        /// </summary>
-        /// <param name="request"></param>
-        public override bool TrySend(IMemcacheRequest request)
+        private void TryReconnect(object dummy)
         {
-            if (request == null)
-                return false;
+            bool reconnected = false;
+            try
+            {
+                reconnected = Initialize();
+            }
+            catch (Exception e)
+            {
+                if (_transportError != null)
+                    _transportError(e);
+                reconnected = false;
+            }
 
-            if (!Initialized && !Initialize())
-                return false;
-
-            return SendRequest(request);
-        }
-
-        private Timer _reconnectTimer;
-        private void PlanReconnect()
-        {
-            _reconnectTimer = new Timer(_ =>
-                {
-                    bool reconnected = false;
-                    try
-                    {
-                        reconnected = Initialize();
-                    }
-                    catch (Exception e)
-                    {
-                        if (_transportError != null)
-                            _transportError(e);
-                        reconnected = false;
-                    }
-
-                    if (reconnected)
-                    {
-                        _reconnectTimer.Dispose();
-                        if (_setupAction != null)
-                            _setupAction(this);
-                    }
-                    else
-                    {
-                        _reconnectTimer.Change(1000, Timeout.Infinite);
-                    }
-                }, null, 1000, Timeout.Infinite);
+            if (reconnected && _setupAction != null)
+                _setupAction(this);
         }
 
         private bool SendRequest(IMemcacheRequest request)
         {
-            var socket = Socket;
+            var socket = _socket;
             try
             {
                 var buffer = request.GetQueryBuffer();
 
-                if (!EnqueueRequest(request, _token.Token))
+                if (!_pendingRequests.TryAdd(request, _queueTimeout, _token.Token))
                     return false;
 
                 int sent = 0;
@@ -351,13 +559,6 @@ namespace Criteo.Memcache.Transport
 
                 return false;
             }
-        }
-
-        public override void PlanSetup()
-        {
-            if (Initialized && _setupAction != null)
-                // the transport is already up => execute now
-                _setupAction(this);
         }
     }
 }
